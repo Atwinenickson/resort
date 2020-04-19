ab180752203981c2c4af72014cac90a015fc66d5
Atwinenickson <atwiinenicxon@gmail.com>
2020-04-19 21:06:55 +0300

    Added room component and laoding component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,25 @@ import Hero from "../components/Hero"
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
 import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
+import Button from '../components/StyledHero'
 
 
 export default function Home() {
     return (
         <React.Fragment>
-        <Hero>
-            <Banner title="luxurious rooms" subtitle="delux 
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="delux 
             rooms starting at $299">
-                <Link to='/rooms' className="btn-primary">
-                    our rooms
+                    <Link to='/rooms' className="btn-primary">
+                        our rooms
                 </Link>
-            </Banner>
-        </Hero>
-        <Services/>
+                </Banner>
+            </Hero>
+            <Services />
+            <FeaturedRooms />
+            <Button>Hello</Button>
         </React.Fragment>
-    )
+    );
 }
 
